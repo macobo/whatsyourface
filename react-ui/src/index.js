371367af61816ac978e1aee9a992e452b762e08a
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2020-03-25 23:48:06 +0200

    Hook webhook into redux

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import { configureStore } from "@reduxjs/toolkit"
+import { Provider } from "react-redux"
+import * as reducers from './reducer'
 import websocket from './websocket'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import './index.css'
+
+const store = configureStore({ reducer: reducers })
+
+ReactDOM.render(
+  <Provider store={store}><App /></Provider>,
+  document.getElementById('root')
+)
 
 websocket.onmessage = (event) => {
   const message = JSON.parse(event.data)
-  console.log(message)
+  store.dispatch(message)
 }
