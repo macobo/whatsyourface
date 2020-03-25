abf6376f3fa777049e3c9e11fe7f1707ec3002df
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2020-03-25 22:00:39 +0200

    Hello world with websockets

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const { Server } = require('ws')
 const path = require('path')
 
 const isDev = process.env.NODE_ENV !== 'production'
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5005
 
 const app = express()
 
@@ -16,6 +16,19 @@ app.get('*', function(request, response) {
   response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'))
 })
 
-app.listen(PORT, function () {
+const server = app.listen(PORT, function () {
   console.error(`Node ${isDev ? 'dev server' : 'cluster worker '+process.pid}: listening on port ${PORT}`)
 })
+
+const websockets = new Server({ server })
+
+websockets.on('connection', (ws) => {
+  console.log('Client connected')
+  ws.on('close', () => console.log('Client disconnected'))
+})
+
+setInterval(() => {
+  websockets.clients.forEach((client) => {
+    client.send(new Date().toTimeString())
+  })
+}, 1000)
