549fffeabc9911c01e9b7924020c55ff3e4b0076
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2020-03-26 23:53:06 +0200

    Add button to capture user picture

--- react-ui/src/UserImage.jsx
+++ react-ui/src/UserImage.jsx
@@ -1,10 +1,20 @@
 import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
+import { captureUserPicture } from './actions'
 
-export default class UserImage extends PureComponent {
+export class UserImage extends PureComponent {
   render = () => (
-    <img
-      src={this.props.user.image || 'https://via.placeholder.com/320x240'}
-      alt={this.props.user.id}
-    />
+    <div class="user-image">
+      <img
+        className="user-image__image"
+        src={this.props.user.image || 'https://via.placeholder.com/320x240'}
+        alt={this.props.user.id}
+      />
+      <div className="user-image__overlay">
+        <button onClick={this.props.captureUserPicture}>Take photo</button>
+      </div>
+    </div>
   )
 }
+
+export default connect(undefined, { captureUserPicture })(UserImage)
